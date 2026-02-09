fe10228d7489d7924a99d689483b842fb0e9f8d7
swatha <13080@holbertonstudents.com>
2026-02-09 09:14:57 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints a sentence
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+putchar('\n');
+
+return (0);
+}
