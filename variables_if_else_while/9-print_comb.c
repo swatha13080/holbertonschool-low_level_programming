17a013ef9f1cbfd89e1e3319595c9e8353fe2a17
swatha <13080@holbertonstudents.com>
2026-02-09 09:50:22 +0000

    comma

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints a sentence
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+	putchar(i);
+	if (i < '9')
+	{
+	putchar(' ');
+	putchar(',');
+	}
+}
+putchar('\n');
+
+return (0);
+}
