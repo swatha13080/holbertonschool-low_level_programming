1e3b068a7a5e9ff8eb2ba74feb2fd5e0059e4cd6
swatha <13080@holbertonstudents.com>
2026-02-09 07:10:19 +0000

    type

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints a sentence
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+printf("Size of a char: %zu byte(s)", sizeof(char));
+printf("Size of an int: %zu byte(s)", sizeof(int));
+printf("Size of a long int: %zu byte(s)", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)", sizeof(long long int));
+printf("Size of a float: %zu byte(s)", sizeof(float));
+return (0);
+}
