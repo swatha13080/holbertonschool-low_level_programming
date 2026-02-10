ada6e37d2142c9858d2ea1670031b0932479117b
swatha <13080@holbertonstudents.com>
2026-02-10 06:27:29 +0000

    9times

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table of 9
+ */
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = i * j;
+
+			if (k > 9)
+				_putchar(k / 10 + '0');
+			else
+				_putchar(' ');
+			_putchar(k % 10 + '0');
+
+			if (j == 9)
+				_putchar('\n');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
