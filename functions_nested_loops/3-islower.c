34ffb7ebc6fc5899415a9c13c519b4598a0ddb9e
swatha <13080@holbertonstudents.com>
2026-02-10 05:04:34 +0000

    submission2

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - Rertuns true if char c is lower
+ * @c: the character to be checked
+ *
+ * Return: returns 0 or 1
+ */
+
+int _islower(int c)
+{
+	return (c > 'Z');
+}
