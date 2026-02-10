b9d1b907134e01e7fe080d7aeec971347b9cea97
swatha <13080@holbertonstudents.com>
2026-02-10 05:40:43 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - Rertuns absolute value of int n
+ * @n: the int to be checked
+ *
+ * Return: returns square root of the square of n
+ */
+
+int _abs(int n)
+{
+	return (n < 0 ? -1 * n : n);
+}
