60ad799f743c419a697211db291a0f683db95690
swatha <13080@holbertonstudents.com>
2026-02-10 03:57:31 +0000

    submission

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,23 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Prints a sentence
+ * print_alphabet - Prints alphabet
  *
  * Return: returns 0
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	int ch;
 
-	return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
 }
