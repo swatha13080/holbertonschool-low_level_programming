42995f163afafa51ec85d9cce976fd01df325057
swatha <13080@holbertonstudents.com>
2026-02-16 11:33:48 +0000

    submit code

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+/**
+ * _isupper - check if char c is uppercase.
+ * @c: the character to be checked.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+	return (c <= 90);
+}
