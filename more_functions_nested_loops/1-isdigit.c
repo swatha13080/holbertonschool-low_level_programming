bb95b85a25761288384926b86040330850ff437a
swatha <13080@holbertonstudents.com>
2026-02-16 11:46:38 +0000

    find digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+/**
+ * _isdigit  - check if char c is digit.
+ * @c: the character to be checked.
+ *
+ * Return: 1 if c is digit, 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
