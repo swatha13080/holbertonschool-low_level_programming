6b7a19ca5f754781b166cf4ec6c416e198d3f74b
swatha <13080@holbertonstudents.com>
2026-02-17 06:20:50 +0000

    modify negation

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,13 +8,17 @@
  */
 int _atoi(char *s)
 {
-	int sign = 0, i = 0, j = 0, result = 0, pow = 1;
+	int sign = 0, i = 0, j = 0, k, result = 0, pow = 1;
 
 	while (s[i])
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			sign = i > 0 && s[i - 1] == '-' ? -1 : 1;
+			/* find sign based on preceding minuses count */
+			for (k = 0; k < i; k++)
+				if (s[k] == '-')
+					sign++;
+			sign = sign % 2 == 0 ? 1 : -1;
 			j = i + 1;
 			while (s[j] && s[j] >= '0' && s[j] <= '9')
 				j++;
@@ -27,8 +31,8 @@ int _atoi(char *s)
 	while (j > i)
 	{
 		result += (s[j - 1] - '0') * pow;
-		pow *= 10;
-		j--;
+		if (--j > i)
+			pow *= 10;
 	}
 
 	return (sign * result);
