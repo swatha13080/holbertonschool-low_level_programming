d77d3bc9748298855c8d0f92c58bc85d7acf9ed4
swatha <13080@holbertonstudents.com>
2026-02-17 09:04:49 +0000

    rev array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+/**
+ * reverse_array - reverses int array.
+ * @a: array to be reversed.
+ * @n: length of a.
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
