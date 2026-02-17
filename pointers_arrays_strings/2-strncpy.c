4dffb364df534c7825de5757efda51ace1f1d220
swatha <13080@holbertonstudents.com>
2026-02-17 08:22:22 +0000

    try this

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (src[i] && i < n)
+	while (i < n)
 	{
-		dest[i] = src[i];
+		if (!src[i])
+			dest[i] = '\0';
+		else
+			dest[i] = src[i];
 		i++;
 	}
-	if (!src[i])
-		dest[i] = '\0';
 
 	return (dest);
 }
