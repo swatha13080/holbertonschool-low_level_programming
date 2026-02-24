410f38f915f5f63888c55d242a7fd750134aa18a
swatha <13080@holbertonstudents.com>
2026-02-24 17:51:19 +0000

    fix

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int test_values(int n, int start);
+int lookup_sqrt(int n, int low, int high);
 
 /**
  * _sqrt_recursion - return sqrt n.
@@ -10,45 +10,35 @@ int test_values(int n, int start);
  */
 int _sqrt_recursion(int n)
 {
-	return (test_values(n, n / 2));
+	if (n < 0)
+		return (-1);
+	else if (n < 2)
+		return (n);
+
+	return (lookup_sqrt(n, 0, n));
 }
 
 /**
- * _test_values - tests values.
- * @n: num.
- * @start: start.
+ * lookup_sqrt - lookup sqrt.
+ * @n: num used.
+ * @low: start.
+ * @high: end.
  *
  * Return: int result.
  */
-int test_values(int n, int start)
+int lookup_sqrt(int n, int low, int high)
 {
-	if (n < 0 || val < n)
+	int mid;
+
+	if (low > high)
 		return (-1);
-	else if (val > n)
-		return (test_values(n, start - 1));
+
+	mid = (low + high) / 2;
+
+	if (n / mid == mid && n % mid == 0)
+		return (mid);
+	else if (n / mid < mid)
+		return (lookup_sqrt(n, low, mid - 1));
 	else
-		return (start);
-}
-
-int sqrt_helper(int n, int low, int high)
-{
-        if (low > high)
-                return (-1);
-
-        int mid = (low + high) / 2;
-
-        if (mid == n / mid && n % mid == 0)
-                return (mid);
-        else if (mid > n / mid)
-                return (sqrt_helper(n, low, mid - 1));
-        else
-                return (sqrt_helper(n, mid + 1, high));
-}
-
-int _sqrt_recursion(int n)
-{
-        if (n < 0)
-                return (-1);
-
-        return (sqrt_helper(n, 0, n));
+		return (lookup_sqrt(n, mid + 1, high));
 }
