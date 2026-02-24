a2a7921fcf2e3ddda8d7b3831059fb26d007ac16
swatha <13080@holbertonstudents.com>
2026-02-24 10:00:57 +0000

    check this later

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -10,7 +10,7 @@ int test_values(int n, int start);
  */
 int _sqrt_recursion(int n)
 {
-	return (test_values(n, n / 2 + 1));
+	return (test_values(n, n / 2));
 }
 
 /**
@@ -22,12 +22,33 @@ int _sqrt_recursion(int n)
  */
 int test_values(int n, int start)
 {
-	int val = start * start;
-
 	if (n < 0 || val < n)
 		return (-1);
 	else if (val > n)
 		return (test_values(n, start - 1));
 	else
 		return (start);
+}
+
+int sqrt_helper(int n, int low, int high)
+{
+        if (low > high)
+                return (-1);
+
+        int mid = (low + high) / 2;
+
+        if (mid == n / mid && n % mid == 0)
+                return (mid);
+        else if (mid > n / mid)
+                return (sqrt_helper(n, low, mid - 1));
+        else
+                return (sqrt_helper(n, mid + 1, high));
+}
+
+int _sqrt_recursion(int n)
+{
+        if (n < 0)
+                return (-1);
+
+        return (sqrt_helper(n, 0, n));
 }
