af7d98bf9ed56e113c7b4f0dc1e79e026061754f
shamsWMM <shamswathas@gmail.com>
2026-03-04 05:18:16 +0000

    create dog struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog owner's name
+ *
+ * Description: a struct of a dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
