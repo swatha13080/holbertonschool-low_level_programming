46ddf0152b31ff4bdf1f739ffe575e522d547003
shamsWMM <shamswathas@gmail.com>
2026-03-09 21:09:42 +0000

    create initial files

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,45 @@
+/**
+ * op_add - add two integers
+ * @a: addend
+ * @b: addend
+ *
+ * Return: sum or exit with error
+ */
+int op_add(int a, int b);
+
+/**
+ * op_sub - subtract an integer from another
+ * @a: minuend
+ * @b: subtrahend
+ *
+ * Return: difference or exit with error
+ */
+int op_sub(int a, int b);
+
+/**
+ * op_mul - multiply two integers
+ * @a: multiplicand
+ * @b: multiplier
+ *
+ * Return: product or exit with error
+ */
+int op_mul(int a, int b);
+
+/**
+ * op_div - divide an integer by another
+ * @a: dividend
+ * @b: divisor
+ *
+ * Return: quotient or exit with error
+ */
+int op_div(int a, int b);
+
+/**
+ * op_mod - modulo divide an integer by another
+ * @a: dividend
+ * @b: modulus
+ *
+ * Return: remainder or exit with error
+ */
+int op_mod(int a, int b);
+
