10321fc3a3c5fbecb155a7bb26c0ec5f6ffeb1bc
shamsWMM <shamswathas@gmail.com>
2026-03-09 10:11:20 +0000

    functions

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function used to print the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
