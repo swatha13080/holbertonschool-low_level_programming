db0bee5d87c863c60698c74c51b68c969ac17c35
shamsWMM <shamswathas@gmail.com>
2026-03-29 03:31:00 +0000

    remove conflict

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-dlistint_t *_create_node(const int n, dlistint_t *next, dlistint_t *prev);
+dlistint_t *create_node(const int n, dlistint_t *next, dlistint_t *prev);
 dlistint_t *_place_last(dlistint_t **h, dlistint_t *last);
 
 /**
@@ -17,7 +17,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (!head)
 		return (NULL);
 
-	last = _create_node(n, NULL, NULL);
+	last = create_node(n, NULL, NULL);
 
 	if (!last)
 		return (NULL);
@@ -32,14 +32,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 }
 
 /**
- * _create_node - create a node
+ * create_node - create a node
  * @n: int value of node
  * @next: pointer to next node
  * @prev: pointer to previous node
  *
  * Return: created node
  */
-dlistint_t *_create_node(const int n, dlistint_t *next, dlistint_t *prev)
+dlistint_t *create_node(const int n, dlistint_t *next, dlistint_t *prev)
 {
 	dlistint_t *node = malloc(sizeof(dlistint_t));
 
