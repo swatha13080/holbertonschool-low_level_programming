ce7d35b3b11ab1b4308af8fdf5526d3d130940e0
shamsWMM <shamswathas@gmail.com>
2026-03-29 03:22:15 +0000

    insert node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,8 @@
 #include "lists.h"
 
+dlistint_t *_find_node_at_index(unsigned int idx, dlistint_t *h);
+dlistint_t *_create_node(int n, dlistint_t *prev, dlistint_t *next);
+
 /**
  * insert_dnodeint_at_index - insert item at index
  * @h: pointer to pointer to head of list
@@ -10,31 +13,69 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 0;
-	dlistint_t *item, *head = *h;
+	dlistint_t *item, *prev, *next;
 
 	if (!h)
 		return (NULL);
 
-	item = malloc(sizeof(dlistint_t));
+	item = _create_node(n, NULL, NULL);
 	if (!item)
 		return (NULL);
+	if (!idx && !*h)
+		;
+	else if (!idx)
+		item->next = *h;
+	else
+	{
+		prev = _find_node_at_index(idx - 1, *h);
+		if (!prev)
+			item = NULL;
+		else
+		{
+			next = prev->next;
+			prev->next = item;
+			item->next = next;
+			item->prev = prev;
+		}
+	}
+	return (item);
+}
 
+/**
+ * _create_node - creates node
+ * @n: node value
+ * @prev: previous node
+ * @next: next node
+ *
+ * Return: created node
+ */
+dlistint_t *_create_node(int n, dlistint_t *prev, dlistint_t *next)
+{
+	dlistint_t *item = malloc(sizeof(dlistint_t));
+
+	if (!item)
+		return (NULL);
 	item->n = n;
+	item->prev = prev;
+	item->next = next;
+	return (item);
+}
 
-	while (head && count != idx)
+/**
+ * _find_node_at_index - get node at index
+ * @idx: index
+ * @h: pointer to head
+ *
+ * Return: pointer to node at index
+ */
+dlistint_t *_find_node_at_index(unsigned int idx, dlistint_t *h)
+{
+	unsigned int count = 0;
+
+	while (h && count != idx)
 	{
 		count++;
-		head = head->next;
+		h = h->next;
 	}
-
-	if (count == idx)
-	{
-		item->prev = head->prev;
-		head->prev = item;
-		item->next = head;
-		return (item);
-	}
-	else
-		return (NULL);
+	return (h);
 }
